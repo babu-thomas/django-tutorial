d1e4149fd09a6d2d36aa88b870d66ab6006384ca
Babu Thomas <babuthomas138@gmail.com>
2018-02-10 12:48:48 +0530

    Use shortcut for showing 404 page

--- first_site/polls/views.py
+++ first_site/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import Http404, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
 from .models import Question
 
 
@@ -10,10 +10,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'polls/detail.html', context)
 
