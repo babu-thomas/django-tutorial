85b4d0f1c4cdc8360201dc4328318a670c2d3b24
Babu Thomas <babuthomas138@gmail.com>
2018-02-10 12:03:45 +0530

    Add placeholders for all views of polls app

--- first_site/polls/urls.py
+++ first_site/polls/urls.py
@@ -3,5 +3,12 @@ from . import views
 
 
 urlpatterns = [
+    # /polls/
     path('', views.index, name='index'),
+    # /polls/3/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # /polls/3/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # /polls/3/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- first_site/polls/views.py
+++ first_site/polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>Welcome to Polls app!</h1>')
+
+
+def detail(request, question_id):
+    return HttpResponse('Question #%s' % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse('Results for question #%s' % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse('Voting page for question#%s' % question_id)
