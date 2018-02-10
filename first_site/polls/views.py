3c2ddf103de9f9b9054367862fd2d5ab1d8689c3
Babu Thomas <babuthomas138@gmail.com>
2018-02-10 13:42:18 +0530

    Implement voting

--- first_site/polls/views.py
+++ first_site/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Choice, Question
 
 
 def index(request):
@@ -20,4 +22,14 @@ def results(request, question_id):
 
 
 def vote(request, question_id):
-    return HttpResponse('Voting page for question#%s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # If selected choice is not valid, redisplay selection form with error message
+        context = {'question': question, 'error_message': "You didn't select a choice."}
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question_id,)))
