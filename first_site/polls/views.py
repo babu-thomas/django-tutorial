03e24521ebd58a71ee544ed9bd62c741097b7556
Babu Thomas <babuthomas138@gmail.com>
2018-02-10 12:45:19 +0530

    Show 404 page when question with given id not in db

--- first_site/polls/views.py
+++ first_site/polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from .models import Question
 
 
@@ -10,7 +10,12 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse('Question #%s' % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 
 def results(request, question_id):
