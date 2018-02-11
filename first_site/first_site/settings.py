cb364504091cf9d97cb088d2b53b7dcfae023ac8
Babu Thomas <babuthomas138@gmail.com>
2018-02-11 20:27:24 +0530

    Add personal app

--- first_site/first_site/settings.py
+++ first_site/first_site/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'first_app.apps.FirstAppConfig',
+    'personal.apps.PersonalConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- first_site/first_site/urls.py
+++ first_site/first_site/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first_app/', include('first_app.urls')),
+    path('', include('personal.urls')),
     path('polls/', include('polls.urls')),
 ]
